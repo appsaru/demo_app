0dc23d01064a97ecbbd376f55c6bdbe065751a5f
appsaru <arunbeee@in.com>
2013-01-28 12:38:35 +0530

    modified users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,8 @@ class UsersController < ApplicationController
   # GET /users.json
   def index
     @users = User.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @users }
-    end
-  end
+end
+      end
 
   # GET /users/1
   # GET /users/1.json
